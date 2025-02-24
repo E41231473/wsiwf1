e4d08ff7accf90444718999bbcf359a96f1bcf3e
E41231473 <e41231473@student.polije.ac.id>
2025-02-24 10:46:02 +0700

    update acara 7

--- resources/views/frontend/layouts/template.blade.php
+++ resources/views/frontend/layouts/template.blade.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>{{comfig('app.name')}}</title>
+  <title>@yield('title')</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="frontend/assets/img/favicon.png" rel="icon">
-  <link href="frontend/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="{{ asset('frontend/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('frontend/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="frontend/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="frontend/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="frontend/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="frontend/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="frontend/assets/css/main.css" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/css/main.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Butterfly
@@ -38,17 +38,51 @@
 
 <body class="index-page">
 
-@include(frontend/layouts.navbar)
+  <header id="header" class="header d-flex align-items-center sticky-top">
+ 
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="{{ asset('frontend/assets/img/logo.png')}}" alt="">
+        <!-- Uncomment the line below if you also wish to use text logo -->
+        <!-- <h1 class="sitename">Butterfly</h1>  -->
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="#hero" class="active">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
 
   <main class="main">
-
-    @yield('content')
-
-    </main>
-
-    <!-- Hero Section -->
-    
+      @yield('content')
+  </main>
 
   <footer id="footer" class="footer">
 
@@ -121,18 +155,18 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="frontend/assets/vendor/php-email-form/validate.js"></script>
-  <script src="frontend/assets/vendor/aos/aos.js"></script>
-  <script src="frontend/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="frontend/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="frontend/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="frontend/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{ asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{ asset('frontend/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
 
   <!-- Main JS File -->
-  <script src="frontend/assets/js/main.js"></script>
-
+  <script src="{{ asset('frontend/assets/js/main.js')}}"></script>
+  @yield('footer')
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,9 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/foo', function () {
-    return "Hello World";
-});
-
-Route::get('user/{id}',function($id){
-    return 'User '.$id;
-});
-
-Route::get('/user/{name?}', function ($name = 'Guest') {
-    return 'Hello, ' . $name;
-});
-
-// Route::get('/home', function () {
-//     return view('home', [
-//         'home' => 'Willy',
-//         'pelajaran' => ['Matematika', 'Fisika', 'Kimia', 'Biologi']
-//     ]);
-// });
-
 Route::get('/home', [HomeController::class, 'index']);
-Route::resource('/home', HomeController::class);
