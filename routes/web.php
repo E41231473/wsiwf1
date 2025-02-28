26962afe220256f1df1eab3a8fa68337267d9e1e
E41231473 <e41231473@student.polije.ac.id>
2025-02-28 09:20:19 +0700

    acara 8

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', [HomeController::class, 'index']);
+
+// Route::group(['namespace' => 'App\Http\Controllers\backend'], function()
+// {
+//     Route::resource('dashboard', 'DashboardController');
+// });
+
+Route::get('/dashboard', [DashboardController::class, 'index'] );
